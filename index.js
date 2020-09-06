c97951fe523ffcd3c8bec061d2e94f7ca5f30ee1
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-06 15:29:03 -0700

    CRUD complete

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 const express = require("express");
 const path = require("path");
-const moment = require("moment");
-const members = require("./Members");
+
+const logger = require("./middleware/logger");
 
 const app = express();
 
 // moment date now
 
-const logger = (req, res, next) => {
-	console.log(
-		`${req.protocol}://${req.get("host")}${
-			req.originalUrl
-		}: ${moment().format()}`
-	);
-	next();
-};
-
 // Init middleware
-app.use(logger);
+//app.use(logger);
 
-// Gets all members
-app.get("/api/members", (req, res) => {
-	res.json(members);
-});
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Members API Routes
+app.use("/api/members", require("./routes/api/members"));
 // app.get("/", (req, res) => {
 // 	res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
