b8ec7d3cdded90aa2c1e7fb6cd98ac4a95a245d4
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-06 14:24:19 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const moment = require("moment");
+const members = require("./Members");
+
+const app = express();
+
+// moment date now
+
+const logger = (req, res, next) => {
+	console.log(
+		`${req.protocol}://${req.get("host")}${
+			req.originalUrl
+		}: ${moment().format()}`
+	);
+	next();
+};
+
+// Init middleware
+app.use(logger);
+
+// Gets all members
+app.get("/api/members", (req, res) => {
+	res.json(members);
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+// app.get("/", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
